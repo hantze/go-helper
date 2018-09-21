a8db211f9bf84aea4feee1b2b40ab5d675b6d685
hantze <hantze.sudarma@payfazz.com>
2018-09-21 13:42:58 +0700

    repackage

--- boiler-plate/internal/helper/db/table.go
+++
@@ -1,7 +0,0 @@
-package db
-
-// Table ...
-type Table struct {
-	Table      string
-	PrimaryKey string
-}
--- boiler-plate/scripts/commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")/.."
-
-find internal/ -type f -name '*.go' -exec goimports -w -v {} \;
-gofmt -r 'α[β:len(α)] -> α[β:]' -w internal;
